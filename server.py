854d23b7b85c5acdc84bd35b3efee7e2da6517a0
yugaltale5 <yugaltale5@gmail.com>
2018-12-07 14:04:26 +0530

    Socket programs in Python.

---
+++ server.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import socket
+s = socket.socket()
+print "socket created"
+port = 12345
+s.bind(('',port))
+print "Socket binded to %s" %(port)
+s.listen(5)
+print "Socket is listening"
+while True:
+    c,addr = s.accept()
+print "Got connected from",addr
+c.send('Thank u for connecting')
+c.close()
