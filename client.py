d212724b560a8955ca1be9fd7c14d9f0f61b3b70
Vedantdavile <37044235+Vedantdavile@users.noreply.github.com>
2019-10-05 14:08:17 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
 import socket
+import time
 s = socket.socket()
 port = 12345
 s.connect(('127.0.0.1',port))
-print "conected to server on port %s" % port
+print("wait for 3 sec")
+time.sleep(3)
+print ("conected to server on port side %s" % port)
 print s.recv(12345)
 s.close()
