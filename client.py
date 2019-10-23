a981af78ef5153edd8773c81f6820918019a646f
salweprakhar <40379050+salweprakhar@users.noreply.github.com>
2019-10-23 21:25:07 +0530

    Update client.py

--- client.py
+++ client.py
@@ -3,6 +3,6 @@ import socket
 s = socket.socket()
 port = 12345
 s.connect(('127.0.0.1',port))
-print "conected to server on port %s" % port
+print "connected to server on port %s" % port
 print s.recv(12345)
 s.close()
