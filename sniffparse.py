970197733abb38e3fcddf373a707a43ccdbf5a13
Yugal Tale <yugaltale5@gmail.com>
2020-10-01 11:41:41 +0530

    Py3 support

--- sniffparse.py
+++ sniffparse.py
@@ -8,7 +8,7 @@ from struct import *
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
 except socket.error , msg:
-    print 'Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    print('Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
     sys.exit()
  
 # receive a packet
@@ -35,7 +35,7 @@ while True:
     s_addr = socket.inet_ntoa(iph[8]);
     d_addr = socket.inet_ntoa(iph[9]);
      
-    print 'Version : ' + str(version) + ' IP Header Length : ' + str(ihl) + ' TTL : ' + str(ttl) + ' Protocol : ' + str(protocol) + ' Source Address : ' + str(s_addr) + ' Destination Address : ' + str(d_addr)
+    print('Version : ' + str(version) + ' IP Header Length : ' + str(ihl) + ' TTL : ' + str(ttl) + ' Protocol : ' + str(protocol) + ' Source Address : ' + str(s_addr) + ' Destination Address : ' + str(d_addr))
      
     tcp_header = packet[iph_length:iph_length+20]
      
@@ -49,7 +49,7 @@ while True:
     doff_reserved = tcph[4]
     tcph_length = doff_reserved >> 4
      
-    print 'Source Port : ' + str(source_port) + ' Dest Port : ' + str(dest_port) + ' Sequence Number : ' + str(sequence) + ' Acknowledgement : ' + str(acknowledgement) + ' TCP header length : ' + str(tcph_length)
+    print('Source Port : ' + str(source_port) + ' Dest Port : ' + str(dest_port) + ' Sequence Number : ' + str(sequence) + ' Acknowledgement : ' + str(acknowledgement) + ' TCP header length : ' + str(tcph_length))
      
     h_size = iph_length + tcph_length * 4
     data_size = len(packet) - h_size
@@ -57,5 +57,5 @@ while True:
     #get data from the packet
     data = packet[h_size:]
      
-    print 'Data : ' + data
-    print
+    print ('Data : ' + data)
+    print()
