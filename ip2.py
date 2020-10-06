3f57453b7920acc04a421c6872c06071d5488db6
Yugal Tale <yugaltale5@gmail.com>
2020-10-06 22:51:12 +0530

    Update ip2.py

--- ip2.py
+++ ip2.py
@@ -3,5 +3,5 @@ s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 print"Enter website address: "
 x=raw_input()
 ip1=socket.gethostbyname(x)
-print "ip address of "+x 
+print ("ip address of "+x )
 print ip1
