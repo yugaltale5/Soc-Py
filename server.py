d626a84172ae61099268913a68ef8d16360d286e
Vedantdavile <37044235+Vedantdavile@users.noreply.github.com>
2020-10-08 00:17:25 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
 import socket
 s = socket.socket()
-print "socket created"
+print ("socket created")
 port = 12345
 s.bind(('',port))
-print "Socket binded to %s" %(port)
+print ("Socket binded to %s" %(port))
 s.listen(5)
-print "Socket is listening"
+print ("Socket is listening")
 while True:
     c,addr = s.accept()
-print "Got connected from",addr
+print ("Got connected from",addr)
 c.send('Thank u for connecting')
 c.close()
