cc6747c177d95556be4a0f6dfd91a1695d1e4d0d
Vedantdavile <37044235+Vedantdavile@users.noreply.github.com>
2020-10-08 00:04:36 +0530

    Update udps.py

--- udps.py
+++ udps.py
@@ -1,7 +1,7 @@
 from socket import *
 s = socket(AF_INET,SOCK_DGRAM)
 s.bind(("",10000))
-print "UDP server started..."
+print ("UDP server started...")
 while True:
     data, addr = s.recvfrom(1000)
 resp = "Get off my lawn!"
