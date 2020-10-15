b19e12a06d008991dd45285904d61b9cb5295013
Nilesh Halge <nileshhalge01@gmail.com>
2020-10-15 10:02:02 +0530

    Update tcpc.py

--- tcpc.py
+++ tcpc.py
@@ -1,5 +1,6 @@
 from socket import *
 s = socket(AF_INET,SOCK_STREAM)
+print ("TCP")
 s.connect(("www.python.org",80)) # Connect
 s.send("GET /index.html HTTP/1.0\n\n") # Send request
 data = s.recv(10000) # Get response
